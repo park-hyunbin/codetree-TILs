5cc175ee3da11a6dc7fe8dbed5af5e499c945a91
Hyunbin Park <89845380+park-hyunbin@users.noreply.github.com>
2024-01-29 20:39:43 +0900

    Title: 살 수 있는 물건 2, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240129/살 수 있는 물건 2/things-able-to-buy-2.py
+++ 240129/살 수 있는 물건 2/things-able-to-buy-2.py
@@ -2,9 +2,8 @@ n = int(input())
 
 if n >= 3000 : 
     print('book')
-elif n >1000 and n <= 3000 : 
+elif n >=1000 and n < 3000 : 
     print('mask')
-elif n < 500 : 
-    print('no')
-else : 
+elif n >= 500 and n <1000 : 
     print('pen')
+else : print('no')
