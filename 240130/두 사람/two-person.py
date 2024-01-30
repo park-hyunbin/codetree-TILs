c6be361cbff6e075754345a7219ea25baf33ca7f
Hyunbin Park <89845380+park-hyunbin@users.noreply.github.com>
2024-01-30 19:50:29 +0900

    Title: 두 사람, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240130/두 사람/two-person.py
@@ -0,0 +1,10 @@
+a, b = map(str, input().split())
+c, d = map(str, input().split())
+
+a = int(a)
+c = int(c)
+
+if (a >= 19 or c >= 19) and (b == 'M' or d == 'M'):
+    print('1')
+else:
+    print('0')
