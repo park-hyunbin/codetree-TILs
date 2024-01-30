b91c701867e98276e1d07f5299dd3f5d4bf6286a
Hyunbin Park <89845380+park-hyunbin@users.noreply.github.com>
2024-01-30 19:43:30 +0900

    Title: 장학금, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240130/장학금/scholarship.py
+++ 240130/장학금/scholarship.py
@@ -1,8 +1,8 @@
 a,b = map(int,input().split())
 
 if a >= 90 and b>= 95 : 
-    print('10')
+    print('100000')
 elif a >= 90 and b <= 95 and b >=90 :
-    print('5')
+    print('50000')
 else : 
     print('0')
