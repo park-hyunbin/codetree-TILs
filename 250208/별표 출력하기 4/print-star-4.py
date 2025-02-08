8bf13df4924a65e386e29291913108e51b54036f
Hyunbin Park <89845380+park-hyunbin@users.noreply.github.com>
2025-02-08 16:43:40 +0900

    Title: 별표 출력하기 4, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250208/별표 출력하기 4/print-star-4.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for i in range(n,0,-1):
+    for j in range(i):
+        print('*', end = ' ')
+    print()
+
+for i in range(n-1):
+    for j in range(i+2):
+        print('*', end = ' ')
+    print()
+    
