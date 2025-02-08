9657bdd50a9c3f56854f1445bb902f35cc949473
Hyunbin Park <89845380+park-hyunbin@users.noreply.github.com>
2025-02-08 15:40:28 +0900

    Title: 소수 판별기, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 250208/소수 판별기/prime-number-discriminator.py
+++ 250208/소수 판별기/prime-number-discriminator.py
@@ -6,13 +6,10 @@ if n == 2 :
 else : 
     for i in range(2,n):
         if n%i ==0 : 
-            break 
-            satisfied = False 
-        else : 
             satisfied = True
+            break
         
-
-if satisfied == False : 
+if satisfied == True : 
     print("C")
 else : 
     print("P")
