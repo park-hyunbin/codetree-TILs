2cf35cedbd9f871c8e2e608b749179e1860f7298
Hyunbin Park <89845380+park-hyunbin@users.noreply.github.com>
2025-02-10 22:34:58 +0900

    Title: 별표 출력하기 11, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250210/별표 출력하기 11/print-star-11.py
+++ 250210/별표 출력하기 11/print-star-11.py
@@ -1,8 +1,8 @@
 n = int(input())
 
-for i in range(n * 2 - 1):  
-    for j in range(n * 2 - 1):
-        if i % (n) == 0 or j % (n) == 0: 
+for i in range(n * 2+1):  
+    for j in range(n * 2+1):
+        if i % (n - 1) == 0 or j % (n - 1) == 0:  
             print('*', end=' ')
         else:
             print(' ', end=' ')
