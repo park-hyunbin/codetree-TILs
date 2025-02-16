90636edbab88b624097e304b24486633695cc3c1
Hyunbin Park <89845380+park-hyunbin@users.noreply.github.com>
2025-02-16 19:55:51 +0900

    Title: 숫자들의 배수, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250216/숫자들의 배수/multiple-of-numbers.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+arr = []
+cnt = 0 
+cntt = 0 
+while True : 
+    cntt += 1
+    arr.append(cntt*n)
+    if cntt*n % 5 == 0 : 
+        cnt +=1 
+        if cnt ==2 : 
+            break
+
+for i in arr : 
+    print(i, end = ' ')
