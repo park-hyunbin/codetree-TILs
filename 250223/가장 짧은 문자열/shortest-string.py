02993e4e4057994cdf3dfff98522c5b7d45f37cb
Hyunbin Park <89845380+park-hyunbin@users.noreply.github.com>
2025-02-23 22:51:25 +0900

    Title: 가장 짧은 문자열, Time: 55ms, Memory: 16MB, Status: Passed - CodeTree

--- 250223/가장 짧은 문자열/shortest-string.py
+++ 250223/가장 짧은 문자열/shortest-string.py
@@ -2,7 +2,7 @@ str1 = input()
 str2 = input()
 str3 = input()
 
-a=min(str1,str2,str3)
-b=max(str1,str2,str3)
+a=min(len(str1),len(str2),len(str3))
+b=max(len(str1),len(str2),len(str3))
 
-print(abs(len(b)-len(a)))
+print(b-a)
