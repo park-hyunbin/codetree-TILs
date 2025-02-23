f72f9e627648d15fe7208cae4b70749580164049
Hyunbin Park <89845380+park-hyunbin@users.noreply.github.com>
2025-02-23 22:46:34 +0900

    Title: 더 긴 문자열, Time: 40ms, Memory: 16MB, Status: Passed - CodeTree

--- 250223/더 긴 문자열/longer-string.py
+++ 250223/더 긴 문자열/longer-string.py
@@ -2,5 +2,7 @@ str_1 = input().split()
 
 if len(str_1[0])>len(str_1[1]):
     print(str_1[0], len(str_1[0]))
-else : 
+elif len(str_1[0])<len(str_1[1]): 
     print(str_1[1],len(str_1[1]))
+else : 
+    print('same')
