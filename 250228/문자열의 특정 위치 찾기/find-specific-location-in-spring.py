7dbbd6f57d60bee72b162ee9adfc8454f13bec75
Hyunbin Park <89845380+park-hyunbin@users.noreply.github.com>
2025-02-28 16:30:08 +0900

    Title: 문자열의 특정 위치 찾기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250228/문자열의 특정 위치 찾기/find-specific-location-in-spring.py
+++ 250228/문자열의 특정 위치 찾기/find-specific-location-in-spring.py
@@ -1,17 +1,12 @@
-string = input().split()  # 공백 기준으로 문자열 리스트 생성
+string = input().split()
 
-if not string:  # 입력이 비어있을 경우 처리
-    print(-1)
-else:
-    concat_str = ' '.join(string)  # 리스트를 하나의 문자열로 변환
-    last_char = string[-1][0]  # 마지막 단어의 첫 번째 문자
+found = False  
 
-    found = False  # 문자를 찾았는지 여부 확인
-    for index, char in enumerate(string[0]):
-        if char == last_char:
-            print(index)
-            found = True
-            break
+for index, char in enumerate(string[0]):
+    if char == string[1]:
+        print(index)
+        found = True
+        break
 
-    if not found:
-        print('No')  # 문자가 없을 경우 -1 출력
+if not found:
+    print('No')
