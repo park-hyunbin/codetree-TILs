f4585b08c246487f893989ef383868fb9385de9c
Hyunbin Park <89845380+park-hyunbin@users.noreply.github.com>
2025-02-28 16:06:10 +0900

    Title: 특정 문자의 유무, Time: 79ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250228/특정 문자의 유무/specific-character-presence.py
@@ -0,0 +1,29 @@
+s = input()
+length = len(s)
+
+exists = False
+exists1 = False
+
+for i in range(length - 1):
+    if s[i:i + 2] == 'ee':
+        exists = True 
+        break
+    else : 
+        exists = False
+        
+
+for i in range(length - 1):
+    if s[i:i + 2] == 'ab':
+        exists1 = True
+        break
+    else : 
+        exists1 = False
+
+if exists == True and exists1 == True : 
+    print("Yes Yes")
+elif exists == True and exists1 == False :
+    print("Yes No")
+elif exists == False and exists1 == True : 
+    print("No Yes")
+else : 
+    prit("No No")
