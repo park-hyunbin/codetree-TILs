8e8dda52c74591bba82a19300cdfd82205c223b3
Hyunbin Park <89845380+park-hyunbin@users.noreply.github.com>
2025-03-02 14:32:22 +0900

    Title: 블럭쌓는 명령2, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250302/블럭쌓는 명령2/block-stacking-commands2.py
@@ -0,0 +1,17 @@
+n,k = map(int,input().split())
+
+segments = [
+    map(int,input().split())
+    for _ in range(k)
+]
+
+blocks = [0]*(n+1)
+
+for a,b in segments : 
+    for i in range(a,b+1):
+        blocks[i] += 1 
+
+max_num = max(blocks)
+print(max_num)
+
+    
