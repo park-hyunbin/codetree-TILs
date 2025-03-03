5ae53ab9434893b2b42bedfc84abd9beb2d23e65
Hyunbin Park <89845380+park-hyunbin@users.noreply.github.com>
2025-03-03 14:27:09 +0900

    Title: 연속되는 수 3, Time: 53ms, Memory: 16MB, Status: Passed - CodeTree

--- 250303/연속되는 수 3/continuous-number3.py
+++ 250303/연속되는 수 3/continuous-number3.py
@@ -1,22 +1,21 @@
 n = int(input())
 
-numbers = [
-    int(input()) 
-    for _ in range(n)]  
+numbers = [int(input()) for _ in range(n)]
 
-max_count = 1 
+if n == 1:
+    print(1)
+    exit()
+
+max_count = 1
 current_count = 1
-current_count_1 = 0
 
 for i in range(1, n):
-    if numbers[i] and numbers[i - 1] > 0 : 
-        current_count += 1
-    elif numbers[i] and numbers[i-1] < 0 : 
-        current_count_1 += 1 
+    if (numbers[i] > 0 and numbers[i - 1] > 0) or (numbers[i] < 0 and numbers[i - 1] < 0):
+        current_count += 1  # 같은 부호일 때 증가
     else:
-        max_count = max(max_count, current_count, current_count_1)  
-        current_count = 1 
+        max_count = max(max_count, current_count)  # 최댓값 갱신
+        current_count = 1  # 새로운 연속 구간 시작
 
-max_count = max(max_count, current_count, current_count_1)
+max_count = max(max_count, current_count)  # 마지막 연속 구간 반영
 
 print(max_count)
